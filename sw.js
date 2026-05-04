70114216c9709be3eaba2a1d29a741a8e6442045
sharifmb92-bit <sharif.mb92@gmail.com>
2026-05-05 01:03:18 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'basha-drive-v3';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
